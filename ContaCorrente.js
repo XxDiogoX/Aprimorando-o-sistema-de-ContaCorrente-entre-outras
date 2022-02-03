f7b6cfa1e70404f13def18072608b319eca834e2
XxDiogoX <diogorodriguesk02@gmail.com>
2022-02-03 01:00:38 -0300

    Sobreposição do método sacar na classe ContaCorrente. porque a operaçao de sacar da ContaCorrente cobra uma taxa por saque

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -5,4 +5,18 @@ export class ContaCorrente extends Conta{
         super(cliente,agencia,numeroConta, 0);
         ContaCorrente.numerosDeConta += 1;
     }
+
+    sacar(valor){
+        const valorSacado = valor + (valor * 5 / 100)
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            console.log(`Valor Sacado: R$ ${valor.toFixed(2)}\nSaldo: ${this._saldo.toFixed(2)}`);
+
+        }else{
+            console.log(`Não foi possível sacar R$ ${valor.toFixed(2)}`);
+        }
+        
+    }
+
+    
 }
--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { Cliente } from "./Cliente.js"
 import {Conta} from "./Conta.js"
 import { ContaCorrente } from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js"
+
+
+
+
